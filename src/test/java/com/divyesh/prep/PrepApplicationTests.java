ec7afea9b161fa14d54433b87c6c9a62a9e13bbf
Divyesh  Shivakumar <divyeshshivakumar@Divyeshs-MacBook-Air.local>
2026-03-12 00:33:49 +0530

    initial commit

---
+++ src/test/java/com/divyesh/prep/PrepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divyesh.prep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
